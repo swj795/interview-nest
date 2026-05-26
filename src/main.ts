9795b06f98d5723de2044781ce7d71f18a34bc40
swj <1061891980@qq.com>
2026-05-26 15:14:29 +0800

    配置swagger

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
