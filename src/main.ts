0d5d4fddd53382d81c8947cfd3d4b5791888032d
swj <1061891980@qq.com>
2026-05-26 15:05:23 +0800

    配置swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('interview api')
+    .setDescription('interview api description')
+    .setVersion('1.0')
+    .addBearerAuth() // 支持bearer token 认证
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
